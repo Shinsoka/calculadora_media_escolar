55eded2b1e3cccff5ba8a92b099a9e0ba17993a4
José Paulo <ritoru.98@gmail.com>
2026-03-11 17:09:04 -0300

    Agora a calculadora aceita vírgula nas notas

--- calculadora_media_escolar.py
+++ calculadora_media_escolar.py
@@ -4,26 +4,25 @@ while continuar == "s":
 			
 			print("Vamos calcular as suas 4 notas do ano, e descobrir se você passou de ano, ficou de recuperação ou foi reprovado! :)")
 
-			nota1	=	float(input("Nota 1: "))
+			nota1	=	float(input("Nota 1: ").replace(",", "."))
 			while nota1 < 0 or nota1 > 10:
 				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota1 = float(input("Nota 1: "))
+				nota1 = float(input("Nota 1: ").replace(",", "."))
 
-			nota2 	=	float(input("Nota 2: "))
+			nota2 	=	float(input("Nota 2: ").replace(",", "."))
 			while nota2 < 0 or nota2 > 10:
 				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota2 = float(input("Nota 2: "))
+				nota2 = float(input("Nota 2: ").replace(",", "."))
 
-			nota3 	=	float(input("Nota 3: "))
+			nota3 	=	float(input("Nota 3: ").replace(",", "."))
 			while nota3 < 0 or nota3 > 10:
 				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota3 = float(input("Nota 3: "))
+				nota3 = float(input("Nota 3: ").replace(",", "."))
 
-			nota4 	=	float(input("Nota 4: "))
+			nota4 	=	float(input("Nota 4: ").replace(",", "."))
 			while nota4 < 0 or nota4 > 10:
 				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota4 = float(input("Nota 4: "))
-
+				nota4 = float(input("Nota 4: ").replace(",", "."))
 			media	= (nota1 + nota2 + nota3 + nota4) / 4
 
 			print("A sua média é:", round(media, 2))
