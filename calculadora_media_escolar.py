983e72a3e3c8d4d0b736cb780c83066eddb0ceb0
José Paulo <ritoru.98@gmail.com>
2026-03-11 17:35:12 -0300

    Código mais simples, sem tantas repetições

--- calculadora_media_escolar.py
+++ calculadora_media_escolar.py
@@ -1,38 +1,25 @@
-continuar = "s"
+print("Vamos calcular as suas 4 notas do ano, e descobrir se você passou de ano, ficou de recuperação ou foi reprovado! :)")
 
-while continuar == "s":
-			
-			print("Vamos calcular as suas 4 notas do ano, e descobrir se você passou de ano, ficou de recuperação ou foi reprovado! :)")
+def pegar_nota(numero):
+	nota = float(input(f"Nota {numero}: ").replace(",", "."))
+	while nota < 0 or nota > 10:
+		print("Nota inválida. Digite um valor entre 0 e 10.")
+		nota = float(input(f"Nota {numero}: ").replace(",", "."))
+	return nota
 
-			nota1	=	float(input("Nota 1: ").replace(",", "."))
-			while nota1 < 0 or nota1 > 10:
-				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota1 = float(input("Nota 1: ").replace(",", "."))
+nota1 = pegar_nota(1)
+nota2 = pegar_nota(2)
+nota3 = pegar_nota(3)
+nota4 = pegar_nota(4)
 
-			nota2 	=	float(input("Nota 2: ").replace(",", "."))
-			while nota2 < 0 or nota2 > 10:
-				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota2 = float(input("Nota 2: ").replace(",", "."))
 
-			nota3 	=	float(input("Nota 3: ").replace(",", "."))
-			while nota3 < 0 or nota3 > 10:
-				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota3 = float(input("Nota 3: ").replace(",", "."))
+media	= (nota1 + nota2 + nota3 + nota4) / 4
 
-			nota4 	=	float(input("Nota 4: ").replace(",", "."))
-			while nota4 < 0 or nota4 > 10:
-				print("Nota inválida. Digite um valor entre 0 e 10.")
-				nota4 = float(input("Nota 4: ").replace(",", "."))
-			media	= (nota1 + nota2 + nota3 + nota4) / 4
+print(f"A sua média é: {media:.2f}")
 
-			print("A sua média é:", round(media, 2))
-
-			if media >= 7:
-				print("Meus parabéns, você foi aprovado!")
-			elif media >= 5:
-				print("Faltou pouco, você está de recuperação, estude bem!")
-			else:
-				print("Você foi reprovado, tente novamente no ano que vem! Ps:Temos desconto para repetentes :)")
-			continuar = input("Deseja continuar? (s/n): ")
-
-print("Programa encerrado")
+if media >= 7:
+	print("Meus parabéns, você foi aprovado!")
+elif media >= 5:
+	print("Faltou pouco, você está de recuperação, estude bem!")
+else:
+	print("Você foi reprovado, tente novamente no ano que vem! Ps:Temos desconto para repetentes :)")
